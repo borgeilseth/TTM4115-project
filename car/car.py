8630b9f4f6569f61a903f54e2d1db89e1c685ce5
LucasRedondo <114664770+LucasRedondo@users.noreply.github.com>
2024-04-15 14:27:41 +0200

    Password in the client before conection

--- car/car.py
+++ car/car.py
@@ -8,6 +8,7 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     client_socket.connect((host, port))
+    client_socket.sendall(b'Send Password') #First the client send the password to conect to the charager
     
     # Send data to the charger
     client_socket.sendall(b'Request to charge')
