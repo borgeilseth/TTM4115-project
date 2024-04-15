337a0469163c15dc89e92f826c663cc84d2dad53
LucasRedondo <114664770+LucasRedondo@users.noreply.github.com>
2024-04-15 14:10:39 +0200

    First look to server conexion protocol

--- charger/charger.py
+++ charger/charger.py
@@ -1,6 +1,29 @@
 from config import *
+import socket
 
 def main():
+host = '0.0.0.0'
+port = 22
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+
+server_socket.listen(1)
+
+print("Waiting for conexion...")
+
+client_socket, client_address = server_socket.accept()
+
+print(f"Conexion from: {client_address}")
+
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+    print("The server had recieved:", data.decode())
+    
+client_socket.close()
+server_socket.close()
     pass
 
 
