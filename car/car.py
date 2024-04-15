13bb68162bc5375a70d9a76bfc841a5784f0011a
LucasRedondo <114664770+LucasRedondo@users.noreply.github.com>
2024-04-15 14:13:00 +0200

    First look to communication from the car

--- car/car.py
+++ car/car.py
@@ -1,6 +1,18 @@
 from config import *
+import socket
 
 def main():
+    host = 'charger.local'  # IP del servidor
+    port = 22  # Puerto de comunicación
+    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    client_socket.connect((host, port))
+    
+    # Send data to the charger
+    client_socket.sendall(b'Request to charge')
+    
+    client_socket.close()
     pass
 
 
