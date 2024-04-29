1d34f47cf59cef2b691d7c1b93d1b8023fac1def
Elias Borge-Ilseth <elias.borge-ilseth@bidbax.no>
2024-04-29 14:35:18 +0200

    connect state-machine and communication

--- charger/charger.py
+++ charger/charger.py
@@ -1,49 +1,149 @@
+from stmpy import Machine, Driver
+import random
+import time
 from config import *
-from car import set_charge_true, set_charge_false
+import json
 import socket
 
-authenticated = True
+global current_charge
 
-if authenticated:
-    set_charge_true()
-else:
-    set_charge_false()
-
-def main():
-    host = 'charger.local'
-    port = 22
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-
-    server_socket.listen(1)
-
-    print("Waiting for conexion...")
-
-    client_socket, client_address = server_socket.accept()
-
-    print(f"Conexion from: {client_address}")
-
-    while True:
-        data = client_socket.recv(1024)
-        if data.strip() == b'Requirenment for conexion': #Our requirenment could be the log-in user or the password
-            print("Conexion Permited")
-            break
-        else:
-            print("Conexion Not Permited")
-            client_socket.close()
-
-    while True:
-        data = client_socket.recv(1024)  # Recibe datos del cliente
-        if not data:
-            break
+class Charger:
+    
+    """
+        possible states:
+            idle
+            validating
+            charging
+            done_charging
             
-    print("We had recieved:", data.decode())
+        possible events:
+            connect: idle -> validating
+            ok: validating -> charging
+            error: validating -> idle
+            done_charging: charging -> done_charging
+            disconnect: done_charging -> idle
+            
+        incoming messages:
+            connect
+            charging
         
-    client_socket.close()
-    server_socket.close()
-    pass
+        outgoing messages:
+            charging
+            disconnect
+            
+    """
+    
+    def check_user(self):
+        "Check user"
+        return True
+
+    def check_user_transition(self):
+        if Charger.check_user(self):
+            self.stm.send("valid_user")
+        else:
+            self.stm.send("invalid_user")
+            
+    def check_settings(self):
+        print ('checking settings')
+
+    def invalid_error(self):
+        print ('not valid user')
+    
+    def stop_charging(self):
+        print ('charging done')
+
+    def on_idle(self):              
+        print('Idling')        
+        
+    def build_message(self) -> dict:
+        if self.stm.state() == "charging":
+            return {
+                "status": "charging",
+                "charging_speed": self.charge_speed
+            }
+        
+        if self.stm.state() == "disconnect":
+            return {
+                "status": "disconnect"
+            }
+    
+    def receive_message(self, message: dict):
+        print("Received from car: \n \t", message)
 
 
-if __name__ == '__main__':
-    main()
+# Transitions
+t0 = {
+    'trigger': '',
+    'source': 'initial',
+    'target': 'idle',
+    'effect': 'on_idle; start_timer("t", 1000)'
+}
+
+#validate
+t1 = {
+    'trigger': 'connect',
+    'source': 'idle',
+    'target': 'validating',
+    'effect': 'check_user_transition'
+}
+
+#start charging
+t2 = {
+    'trigger': 'valid_user',
+    'source': 'validating',
+    'target': 'charging',
+    'effect': 'check_settings; charge'
+}
+
+#error
+t3 = {
+    'trigger': 'invalid_user',
+    'source': 'validating',
+    'target': 'idle',
+    'effect': 'invalid_error'
+}
+
+#done charging
+t4 = {
+    'trigger': 'done_charging',
+    'source': 'charging',
+    'target': 'idle',
+    'effect': 'stop_charging'
+}
+
+def send_message(s, message):
+    serialized_message = json.dumps(message)
+    s.sendall(serialized_message.encode())
+    
+def start_server(host='127.0.0.1', port=65432):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"Server started and listening on {host}:{port}")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                received_message = json.loads(data.decode())
+                charger.receive_message(received_message)
+                time.sleep(1)
+                response = charger.build_message()
+                send_message(conn, response)
+                
+
+# State machine for the charger
+charger = Charger()
+
+machine = Machine(name='charger', transitions=[t0, t1, t2, t3, t4], obj=charger)
+charger.stm = machine
+
+driver = Driver()
+driver.add_machine(machine)
+driver.start()
+
+
+if __name__ == "__main__":
+    start_server()
--- charger/config.py
+++ charger/config.py
@@ -2,4 +2,22 @@
 CHARGER_IP = '10.0.1.1'
 CHARGER_PORT = 12345
 
-Charging_percent = 80
+MAX_CHARGE_PERCENTAGE = 80
+CHARGING_SPEED = 1
+
+USERS = [
+    {
+        'UUID': '1234-5678-9101',
+        'name': 'Elias',
+        'valid_payment': True,
+        'cars': [
+            {
+                'WIN': '1234567890ABCDEF',
+                'nickname': 'Elbil',
+                'make': 'Tesla',
+                'model': 'Model S',
+                'year': 2019
+            }
+        ]
+    }
+]
