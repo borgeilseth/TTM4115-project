94f61c03f50deb375d400dcc3fd52559d6a12533
sondrpe <sondrpe@stud.ntnu.no>
2024-05-02 13:36:10 +0200

    server finpuss

--- interface/server.py
+++ interface/server.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # grid_level = 50 # % of the grid's capacity used
-API_ENDPOINT = "http://192.168.227.109:5001"
+API_ENDPOINT = "http://127.0.0.1:5001"
 
 
 # sending post request and saving response as response object
@@ -23,32 +23,26 @@ def main():
 
 if __name__ == '__main__':
     database = get_data()
-    svar = input("Do you want to print the user database? (y/n)")
+    svar = input("Do you want to print the user database? (y/n) ")
     if svar == 'y':
         print(database)
 
-    oppdatere = input("Do you want to update the user database? (y/n)")
+    oppdatere = input("Do you want to update the user database? (y/n) ")
     if oppdatere == 'y':
-        allow_charging = input("Do you want to allow charging? (y/n)")
-        username = input("Enter car to allow charging: ")
+        allow_charging = input("Do you want to allow charging? (y/n) ")
+        allowed_cars = input("Enter car(s) to allow charging (space separated): ") # 1 2 3 4 
         max_charge_percentage = input("Enter max charge percentage: ")
-        max_charge_percentage = max_charge_percentage if max_charge_percentage else None
+        max_charge_percentage = int(max_charge_percentage) if max_charge_percentage else None
         charging_speed = input("Enter charging speed: ")
         charging_speed = int(charging_speed) if charging_speed else None
 
         data = {}
-        if username:
-            old_data_cars = database['allowed_cars']
-            old_data_cars.append(username)
-            data['allowed_cars'] = old_data_cars
-            list_of_allowed_cars = database['allowed_cars']
-            if username not in list_of_allowed_cars:
-                list_of_allowed_cars.append(username)
-                index = list_of_allowed_cars.index(username)
+        if allowed_cars:
+            data['allowed_cars'] = allowed_cars.split()
         if allow_charging == 'y':
-            data['allow_charging'] = 'True'
+            data['allow_charging'] = True
         if allow_charging == 'n':
-            data['allow_charging'] = 'False'
+            data['allow_charging'] = False
         if max_charge_percentage is not None:
             data['max_charge_percentage'] = max_charge_percentage
         if charging_speed is not None:
