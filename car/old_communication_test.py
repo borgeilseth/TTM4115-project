1518d2ee72991fe98259e5cb981148cc9033fd91
Malin Ludvigsen <malin.ludvigsen@hotmail.com>
2024-05-02 11:26:08 +0200

    lights

--- car/old_communication_test.py
+++ car/old_communication_test.py
@@ -1,19 +1,20 @@
 import socket
 import json
 import time
+import math
 from config import *
-import threading
-# from sense_hat import SenseHat
+from sense_hat import SenseHat
 
-# sense = SenseHat()
-
-dissalowed = False
-
+sense = SenseHat()
+green = (0,255,0)
+red = (255,0,0)
+pink = (255,0,255)
+blue = (0, 191, 255)
 
 class Car():
     """Car class for handling car logic and state
 
-    Can have three states:
+    Can have two states:
         idle
             When the car is not connected to the charger
         charging
@@ -40,17 +41,34 @@ class Car():
             "current_charge": self.current_charge,
             "capacity": MAX_CHARGE_CAPACITY
         }
+    
+    def turn_on_led_rows(number_rows):
+        number_rows = max(1, min(number_rows, 8))
+        for i in range(number_rows):
+            for j in range(8):
+             sense.set_pixel(j, i, 0, 255, 0)
 
     def refresh_sense_led(self):
         global sense
         # Change the sense led color according to the charge and state
+        if self.state == "idle":
+            sense.clear(red)
 
-        pass
+        else:
+            percentage = self.current_charge/MAX_CHARGE_CAPACITY
+            number_of_pixels_on = percentage * 64
+
+            for i in math.floor(number_of_pixels_on):
+                x = i % 8
+                y = i % 8
+                sense.set_pixel(x, y, 0, 255, 0)
 
     def update_charge(self, change):
         self.current_charge += change
         if self.current_charge >= MAX_CHARGE_CAPACITY:
             self.current_charge = MAX_CHARGE_CAPACITY
+        if self.current_charge <= MAX_CHARGE_CAPACITY * CHARGE_TRESHOLD / 100 and self.state == "disconnected":
+            self.state = "idle"
         elif self.current_charge < 0:
             self.current_charge = 0
         self.refresh_sense_led()
@@ -60,29 +78,20 @@ class Car():
         self.refresh_sense_led()
 
     def receive_message(self, message: dict):
-        print(f"Received message: {message}")
         if not message:
             return True
         elif message["status"] == "charging":
             self.update_charge(message.get("charging_speed", 0))
-            self.set_state("charging")
-            print(f"Current charge: {
-                  self.current_charge}, State: {self.state}")
+            print(f"Current charge: {self.current_charge}")
             return True
         elif message["status"] == "disconnect":
-            threading.Thread(target=dissalow_timeouts).start()
+            self.state = "disconnected"
+            print("Disconnected from charger")
             return False
         return True
 
 
 car = Car()
-
-
-def dissalow_timeouts():
-    global dissalowed
-    dissalowed = True
-    time.sleep(10)
-    dissalowed = False
 
 
 def send_message(sock, message):
@@ -94,16 +103,15 @@ def send_message(sock, message):
 
 
 def start_client(server_host='127.0.0.1', server_port=65439):
-    global dissalowed
     while True:
-        print(f"Current charge: {car.current_charge}, State: {car.state}")
+        print(f"Current charge: {car.current_charge}")
 
-        if not dissalowed:
+        if not car.state == "disconnected":
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
                     sock.connect((server_host, server_port))
-                    # print("Charging")
+                    print("Charging")
 
                     send_message(sock, car.build_connect_message())
                     while True:
@@ -122,7 +130,6 @@ def start_client(server_host='127.0.0.1', server_port=65439):
             except socket.error:
                 pass
 
-        car.set_state("idle")
         car.update_charge(DISCHARGE_RATE)
         time.sleep(1)
 
